943d628d006bbad4a858f617385b5d607eb0a255
simson613 <simson0613@gmail.com>
2021-08-18 22:34:44 +0900

    Add CreateAccount swagger example

--- auth/domain/dto/account.go
+++ auth/domain/dto/account.go
@@ -1,11 +1,11 @@
 package dto
 
 type CreateAccount struct {
-	Id          string `json:"id"`
-	Password    string `json:"password"`
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	PhoneNumber string `json:"phone_number"`
-	WorkCode    int    `json:"work_code"`
-	HireDate    string `json:"hire_date"`
+	Id          string `json:"id" example:"simson"`
+	Password    string `json:"password" example:"1234"`
+	Name        string `json:"name" example:"홍길동"`
+	Email       string `json:"email" example:"abc@sample.com"`
+	PhoneNumber string `json:"phone_number" example:"010-1234-1234"`
+	WorkCode    int    `json:"work_code" example:"1"`
+	HireDate    string `json:"hire_date" example:"2021-08-17 09:00:00"`
 }
